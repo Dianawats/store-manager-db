419134b434170fcfffd8690e70ce64521e7ab20f
Dianawats <dianakiznaki@gmail.com>
2018-10-29 20:59:48 +0300

    add input validation

--- app/validation.py
+++ app/validation.py
@@ -5,7 +5,7 @@ class Validator:
     """
     validation class to add product.
     """
-    def user_validator(self, username, contact, role, password):
+    def user_validator(self, username, phone, role, password):
         """
         Method validates the user input and return appropriate message
         """
@@ -15,6 +15,14 @@ class Validator:
             return "username is missing"
         if not re.match(r"^[a-zA-Z\d]+[-_])*[a-zA-Z\d*]+$)", username):
             return "username should contain no white spaces"
+        if not re.match("^\d{4}-\d{6}$",phone):
+            return "phone format should be [07zz-zzzzzz],in digits without white spaces"
+        if (role != "admin" and role != "attendant"):
+            return "role should either be admin or attendant"    
+        if len(username) < 4:
+            return "username should be more than 3 characters long"
+        if not password:
+            return "password is missing"
         
 
     def validate_product_inputs(self, product_name, quantity, price):
@@ -44,7 +52,16 @@ class Validator:
         if int(price) < 1:
             return "price should be greater than zero or more"    
         if price == " ":
-            return "price is missing"    
+            return "price is missing" 
+
+    def login_validator(self, username, password):
+        """
+        Method validates the login input and return appropriate message
+        """
+        if not username:
+            return "username is missing"
+        if not password:
+            return "password is missing"    
         
     def validate_input_type(self, input):
         try:
