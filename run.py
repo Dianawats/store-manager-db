b90120628bf748d73050d4268d904c9be0bdb5a4
Dianawats <dianakiznaki@gmail.com>
2018-10-29 18:05:30 +0300

    add db connection for setup

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from app import app
+from app.db.dbmanager import DatabaseConnection
 
 if __name__ == "__main__":
+    dbUtils = DatabaseConnection()
+    dbUtils.create_tables()
     app.run(debug=True)
