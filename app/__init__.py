9aaa48782b159e875a66f3b6987c1f380e962c94
Dianawats <dianakiznaki@gmail.com>
2018-10-30 19:39:25 +0300

    update init file

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 #create an object of flask using a unique name
 app = Flask(__name__)
 
-from app.auth.authenticate import auth_blueprint
+from app.authentication.authenticate import auth_blueprint
 from app.views import views_blueprint
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(views_blueprint)
