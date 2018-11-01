dd70f94bf7ea26d1a4731fae07d076ce3b2d2607
Dianawats <dianakiznaki@gmail.com>
2018-11-01 20:23:27 +0300

    add error handlers

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
 from app import app
 from app.db.databmanager import DatabaseConnection
+from flask import jsonify
+import os
+
+
+@app.errorhandler(405)
+def url_not_found(error):
+    return jsonify({'message': "Requested URL is invalid"}),405
+
+@app.errorhandler(404)
+def content_not_found(error):
+    return jsonify({'message': "Requested url is not found"}),404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({'message': "Internal server error"}),500
 
 if __name__ == "__main__":
     dbUtils = DatabaseConnection()
